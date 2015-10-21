55f7579abdab190e9d743b535365bd9a97343035
Krzysztof Bartosz <krzysztofbartosz12@gmail.com>
2015-10-21 20:50:26 +0200

    parsowanie krasinskiego i proba zapisu do bazy (nieudana)

--- application/controllers/krasinskiego.php
+++ application/controllers/krasinskiego.php
@@ -12,30 +12,30 @@ class Krasinskiego extends CI_Controller {
 }
 
 $AirQuality = file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/default.aspx');
-/*
+
 $PM10 = file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/PylPM10.aspx');
 $PM25 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/PylPM25.aspx');
 $NO2 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/Dwutlenekazotu.aspx');
 $SO2 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/Dwutleneksiarki.aspx');
 $CO =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/tlenekwegla.aspx');
-*/
+
 $data['quality'] = $AirQuality->find(".right",0);
-/*$data['PM10'] = $PM10->find(".right",0);
+$data['PM10'] = $PM10->find(".right",0);
 $data['PM25'] = $PM25->find(".right",0);
 $data['N02'] = $NO2->find(".right",0);
 $data['SO2'] = $SO2->find(".right",0);
 $data['CO'] = $CO->find(".right",0);
 
-*/
+
 
 
 $data = array(
                 'quality' => $this->input->post('jakosc_powietrza'),
-                /*'PM10' => $this->input->post('pm10'),
+                'PM10' => $this->input->post('pm10'),
                 'PM25' => $this->input->post('pm25'),
                 'N02' => $this->input->post('dwutlenek_azotu'),
                 'S02' => $this->input->post('dwutlenek_siarki'),
-                'CO' => $this->input->post('tlenek_wegla'),*/
+                'CO' => $this->input->post('tlenek_wegla'),
             );
 
             $this->krasinskiego_m->insert($data);
