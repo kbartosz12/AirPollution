c9a3363e80a6ae7dce3329f338e1eabf579fc5c2
Krzysztof Bartosz <krzysztofbartosz12@gmail.com>
2015-11-08 14:46:53 +0100

    poprawione pobieranie + prawie udany zapis do db

--- application/controllers/krasinskiego.php
+++ application/controllers/krasinskiego.php
@@ -1,7 +1,8 @@
 <?php
-include_once('C:\xampp\htdocs\AirPollution\simple_html_dom');
+/*include_once('..\AirPollution\simple_html_dom');
 require('simple_html_dom.php');
 
+*/
 
 class Krasinskiego extends CI_Controller {
     
@@ -9,10 +10,57 @@ class Krasinskiego extends CI_Controller {
         parent::__construct();
         $this->load->model('krasinskiego_m');        
     }
+    
+ public function index() {
+     echo "Jakosc powietrza:";
+     $data = array();
+    // include_once('C:\xampp\htdocs\AirPollution\application\libraries\simple_html_dom.php');
+    // include('C:\xampp\htdocs\AirPollution\application\libraries\simple_html_dom.php');
+     //require_once('C:\xampp\htdocs\AirPollution\application\libraries\simple_html_dom.php');
+     $this->load->library('simple_html_dom');
+     $quality = file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/default.aspx');
+     $PM10 = file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/PylPM10.aspx');
+     $PM25 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/PylPM25.aspx');
+     $NO2 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/Dwutlenekazotu.aspx');
+     $SO2 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/Dwutleneksiarki.aspx');
+     $CO =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/tlenekwegla.aspx');
+     //echo $data['quality'] = $quality->find(".right",0);
+     /*echo $data['PM10'] = $PM10->find(".right",0);
+     echo $data['PM25'] = $PM25->find(".right",0);
+     echo $data['NO2'] = $NO2->find(".right",0);
+     echo $data['SO2'] = $SO2->find(".right",0);
+     echo $data['CO'] = $CO->find(".right",0);*/
+
+     echo  $data = $quality->find(".right",0);
+     
+     var_dump($data);
+     
+    
+
+/*
+    $data = array(
+                    'quality' => $this->input->post('quality'),
+                    'PM10' => $this->input->post('PM10'),
+                    'PM25' => $this->input->post('PM25'),
+                    'NO2' => $this->input->post('NO2'),
+                    'SO2' => $this->input->post('SO2'),
+                    'CO' => $this->input->post('CO'),
+                );
+                */
+                
+    $this->load->model('krasinskiego_m');
+     $this->krasinskiego_m->insert($data);
+                
+ }
 }
 
-$AirQuality = file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/default.aspx');
 
+
+//$AirQuality = new simple_html_dom();
+
+//$AirQuality = file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/default.aspx');
+
+/*
 $PM10 = file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/PylPM10.aspx');
 $PM25 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/PylPM25.aspx');
 $NO2 =  file_get_html('http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/Dwutlenekazotu.aspx');
@@ -39,5 +87,7 @@ $data = array(
             );
 
             $this->krasinskiego_m->insert($data);
+*/
+
 
 ?>
