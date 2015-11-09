3492aa3b5c59cbfd84019beda9d136eaeb21f2d2
Krzysztof Bartosz <krzysztofbartosz12@gmail.com>
2015-11-09 21:25:29 +0100

    wszystko działa

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('krasinskiego_m');        
+    }
 	/**
 	 * Index Page for this controller.
 	 *
@@ -20,6 +25,20 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+            $this->show();
 	}
+        
+        public function show() {
+     
+     $dane = $this->krasinskiego_m->get_all();
+     $data['dane'] = $dane;
+     
+     $this->load->helper('url');
+     $this->load->view('header');
+     //$this->load->view('welcome_message',$data);
+      $this->load->view('welcome_message',$data);
+      
+      $this->load->view('footer');
+     
+ }
 }
